67c2a0d92a8966d51e3a734d5fa2d2b123777352
Matt Bruton <mb@deliowealth.com>
2016-12-01 16:08:38 +0000

    Updated the autoloader

--- classes/bundle_vendor.php
+++ classes/bundle_vendor.php
@@ -12,7 +12,7 @@ namespace adapt\vendor{
         
         public function boot(){
             if (parent::boot()){
-                require(ADAPT_PATH . "{$this->name}/{$this->name}-{$this->version}/libraries/vendor_autoloader.php");
+                require_once(ADAPT_PATH . "{$this->name}/{$this->name}-{$this->version}/libraries/vendor_autoloader.php");
                 spl_autoload_register('vendor_autoloader');
                 return true;
             }
